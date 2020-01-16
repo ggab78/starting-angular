278d666c5202103a43108332429830d3125fdb85
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-01-16 09:40:08 +0100

    using index with ngFor

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,14 +9,27 @@ export class ServerComponent {
   serverId = 10;
   serverStatus = '';
   userName = '';
+  detailText = '';
+  details = ['detail 1', 'detail 2'];
+
 
   constructor() {
-    this.serverStatus = Math.random() > 0.5 ? 'online' : 'ofline';
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  onAddDetail() {
+    this.details.push(this.detailText);
+    this.detailText = '';
+
+  }
+  getDetailColor(i) {
+    return i >= 5 ? 'blue' : 'white';
   }
 
   getColor() {
     return this.serverStatus === 'online' ? 'green' : 'red';
   }
+
 
   getServerStatus() {
     return this.serverStatus + ' from method';
